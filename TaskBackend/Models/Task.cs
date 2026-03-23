fa3d026da885d2abc21b1b3147a0aed7cc426d7a
Simppa <simo.siren@careeria.fi>
2026-03-23 13:32:19 +0200

    Add project files.

---
+++ TaskBackend/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskBackend.Models;
+
+public partial class Task
+{
+    public int TaskId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int Status { get; set; }
+
+    public DateTime? StatusChanged { get; set; }
+
+    public int Priority { get; set; }
+}
---
+++ TaskBackend/Models/TaskDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskBackend.Models;
+
+public partial class TaskDbContext : DbContext
+{
+    public TaskDbContext()
+    {
+    }
+
+    public TaskDbContext(DbContextOptions<TaskDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DUUNIKONE\\SQLEXPRESS; Database=TaskDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.Property(e => e.Description).HasMaxLength(1000);
+            entity.Property(e => e.StatusChanged).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
